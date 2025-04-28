1bacb2bdda28f58e8b874c1b8b20222243d745eb
Kaviiyyappa <93697664+Kaviiyyappa@users.noreply.github.com>
2025-04-28 07:59:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,7 +48,7 @@ REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
 
 # This Is Your Bot Support Group Id , Here Bot Will Not Give File Because This Is Support Group.
-support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002223209081')
+support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002602051457')
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 
 # This Channel Is For /batch command file store.
@@ -88,9 +88,9 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/IKBRYTMOVIES')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+zirAz8A0qZM5MWVl')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+CXdSFgRVbERmZDBl')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'IKBRYTMOVIES') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+zirAz8A0qZM5MWVl') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'tg://settings')
 
 # True Or False
