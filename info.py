a91fc7535e6ca017fc88cfa4f39258d44d840ffa
Kaviiyyappa <93697664+Kaviiyyappa@users.noreply.github.com>
2025-04-28 07:20:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -90,7 +90,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/IKBRYTMOVIES')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+CXdSFgRVbERmZDBl')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+9ae8Fn5CGMZiN2U1') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/IKBRYTMOVIES') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'tg://settings')
 
 # True Or False
