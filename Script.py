71bf0122e6d0864978ea4e8b322cc7930844f776
Kaviiyyappa <93697664+Kaviiyyappa@users.noreply.github.com>
2025-05-05 12:02:41 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -3,10 +3,11 @@
 # Ask Doubt on telegram @KingVJ01
 
 class script(object):
-    START_TXT = """<b><blockquote>ʜᴇʟʟᴏ {} 👋,</blockquote>
-    
-ɪ ᴀᴍ ʟᴀᴛᴇꜱᴛ ᴀᴅᴠᴀɴᴄᴇᴅ ᴀɴᴅ ᴘᴏᴡᴇʀꜰᴜʟ ᴀᴜᴛᴏ ꜰɪʟᴛᴇʀ ʙᴏᴛ. ʏᴏᴜ ᴄᴀɴ ᴜꜱᴇ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴛᴏ ᴇᴀʀɴ ᴜɴʟɪᴍɪᴛᴇᴅ ᴍᴏɴᴇʏ...💸</b>"""
-
+    START_TXT = """<b><blockquote>Hᴇʏ {},🍁</blockquote>\n
+✦ ᴍʏ ɴᴀᴍᴇ ɪꜱ #IKBRYTLinkz ,
+✦ ɪ ᴀᴍ ʙᴇꜱᴛ ᴀᴜᴛᴏ ғɪʟᴛᴇʀ ʙᴏᴛ
+✦ ɪ ᴀᴍ ᴜꜱɪɴɢ ʙᴇꜱᴛ ᴀɪ ꜱᴇᴀʀᴄʜ ᴇɴɢɪɴᴇ
+ !!</b>"""
     CLONE_START_TXT = """<b><blockquote>ʜᴇʟʟᴏ {}, ᴍʏ ɴᴀᴍᴇ <a href=https://t.me/{}>{}</a></blockquote>
     
 ɪ ᴀᴍ ᴀᴅᴠᴀɴᴄᴇ ᴀɴᴅ ᴘᴏᴡᴇʀғᴜʟʟ ᴀᴜᴛᴏғɪʟᴛᴇʀ ʙᴏᴛ ᴡɪᴛʜ ᴀᴍᴀᴢɪɴɢ ғᴇᴀᴛᴜʀᴇs ᴊᴜsᴛ ᴛʏᴘᴇ ᴡʜᴀᴛ ʏᴏᴜ ᴡᴀɴᴛ ᴛʜᴇɴ sᴇᴇ ᴍʏ ᴘᴏᴡᴇʀ 💘</b>"""
